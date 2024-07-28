91600bcc6728d42ef9f34739798a6a7582e31f2d
Tuncay Ekmez <tuncayekmez@gmail.com>
2024-07-28 14:06:06 +0300

    first commit

---
+++ src/main/java/com/tuncayekmez/springtodobackend/SpringTodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tuncayekmez.springtodobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTodoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTodoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuncayekmez/springtodobackend/SpringTodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuncayekmez.springtodobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTodoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
